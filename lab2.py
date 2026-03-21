0b6141c1ae19537d202f2931cece32a9b829a402
Rahul Hegde <rgh9883@rit.edu>
2026-03-21 18:59:00 -0400

    implement resolution

--- lab2.py
+++ lab2.py
@@ -1,23 +1,6 @@
 import sys
 import re
 
-class Clause:
-    def __init__(self, literals):
-        self.literals = literals
-    
-    def __repr__(self):
-        return '{' + ', '.join(map(repr, self.literals)) + '}'
-    
-    def __eq__(self, other):
-        if not isinstance(other, Clause): return False
-        return frozenset(self.literals) == frozenset(other.literals)
-    
-    def __hash__(self):
-        return hash(frozenset(self.literals))
-    
-    def is_empty(self):
-        return len(self.literals) == 0
-
 class Literal:
     def __init__(self, name, args, negated=False):
         self.name = name
@@ -39,9 +22,26 @@ class Literal:
     
     def complement(self):
         return Literal(self.name, self.args, not self.negated)
+
+class Clause:
+    def __init__(self, literals: list[Literal]):
+        self.literals = literals
     
+    def __repr__(self):
+        return '{' + ', '.join(map(repr, self.literals)) + '}'
+    
+    def __eq__(self, other):
+        if not isinstance(other, Clause): return False
+        return frozenset(self.literals) == frozenset(other.literals)
+    
+    def __hash__(self):
+        return hash(frozenset(self.literals))
+    
+    def is_empty(self):
+        return len(self.literals) == 0
+
 class Term:
-    VAR, CONST, FUNC = 'const', 'var', 'func'
+    VAR, CONST, FUNC = 'var', 'const', 'func'
 
     def __init__(self, type, name, args=None):
         self.type = type
@@ -67,6 +67,39 @@ class Term:
         return hash((self.type, self.name, tuple(self.args)))
     
 
+def pl_resolve(c1: Clause, c2: Clause) -> list[Clause]:
+    resolvents = []
+    for lit1 in c1.literals:
+        resolve = []
+        comp = lit1.complement()
+        for lit2 in c2.literals:
+            if comp == lit2:
+                resolve = ([l for l in c1.literals if l != lit1] +
+                            [l for l in c2.literals if l != lit2])
+                resolvents.append(resolve)
+                break
+    return resolvents
+
+
+def pl_resolution(clauses: list[Clause]):
+    known = set(clauses)
+    while True:
+        new = []
+        for ci in clauses:
+            for cj in clauses:
+                for res in pl_resolve(ci, cj):
+                    if res.is_empty():
+                        return False # unsatisfiable
+                    if res not in known:
+                        known.add(res)
+                        new.append(res)
+        if not new:
+            return True # satisfiable
+        clauses.extend(new)
+                
+                
+    
+
 def parse_term(str, vars):
     func = re.match(r'^(\w+)\((.+)\)$', str)
     if func:
@@ -80,12 +113,13 @@ def parse_term(str, vars):
     
     return Term(Term.CONST, str)
 
+
 def parse_lit(str, vars):
     negated = str.startswith('!')
     if negated:
         str = str[1:]
     
-    match = re.match(r'^(\w+)\(((.*))\)$', str)
+    match = re.match(r'^(\w+)\((.*)\)$', str)
     if match:
         name = match.group(1)
         arg_strs = match.group(2).split(',')
@@ -95,6 +129,7 @@ def parse_lit(str, vars):
         args = []
 
     return Literal(name, args, negated)
+
 
 def parse_cnf(filepath):
     with open(filepath) as f:
@@ -116,6 +151,7 @@ def parse_cnf(filepath):
         clauses.append(c)
     
     return clauses
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
